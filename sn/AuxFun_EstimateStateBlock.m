c2c6d0a43c3bd54d2e10f5050294f442e1726a88
Mahsh <542246946@qq.com>
2020-06-02 00:28:52 +0800

    update

--- sn/AuxFun_EstimateStateBlock.m
+++
@@ -1,9 +0,0 @@
-function [estimate_state_block]=AuxFun_EstimateStateBlock(estimate_state_series_block,node_num)
-%找出估计出来的状态块
-[threefold_samples_num,folds]=size(estimate_state_series_block);
-fold_samples_num=threefold_samples_num/3;
-timeslot_perfold=fold_samples_num/node_num;
-estimate_state_block=estimate_state_series_block(1:fold_samples_num,:);
-estimate_state_block=reshape(estimate_state_block,[node_num,timeslot_perfold*folds]);
-
-end
